35ea5675cb4fe597733de28d25589dbb6529c9b6
Q <quintonparker@gmail.com>
2013-12-11 10:25:40 +0200

    Better php-lint for mod'ed git phps

--- scripts/bin/php-lint-git.sh
+++ scripts/bin/php-lint-git.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-git diff --name-only | grep -E ".*\.ph(p|html)$" | while read line
+git diff --name-only --relative | grep -E ".*\.ph(p|html)$" | while read line
 do
     php -l "$line"
 done
