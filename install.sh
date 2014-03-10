e7a7a28a8d215652ee4780f76a59b7f531da207f
Q <quintonparker@gmail.com>
2014-03-10 11:29:22 +0000

    Make installer portable to ubuntu linux. Bit of a hack

--- install.sh
+++ install.sh
@@ -15,27 +15,48 @@ git submodule update --init --recursive
 cd "$DOTFILES_DIR/scripts/bin/"
 pwd
 
-./install-homebrew.sh
-./install-homebrew-cask.sh
+if [ `uname` == "Darwin" ]
+then
+    ./install-homebrew.sh
+    ./install-homebrew-cask.sh
+fi
 
 echo "Installing gnu stow (dotfile deploy tool)..."
-brew install stow
+
+if [ `uname` == "Darwin" ]
+then
+    brew install stow
+else
+    sudo apt-get install stow
+fi
 
 echo "Symlinking in dot files..."
-stow --ignore="\.DS_Store" -d $DOTFILES_DIR -t $HOME -v 1 -S zsh -S vim -S git -S ctags -S scripts -S screen
+if [ `uname` == "Darwin" ]
+then
+    stow --ignore="\.DS_Store" -d $DOTFILES_DIR -t $HOME -v 1 -S zsh -S vim -S git -S ctags -S scripts -S screen
+else
+    stow -d $DOTFILES_DIR -t $HOME --verbose=1 zsh vim git ctags scripts screen
+fi
 
 cd "$DOTFILES_DIR/scripts/bin/"
 pwd
 
-./install-homebrew-formulas.sh
-./install-homebrew-cask-formulas.sh
-./install-quick-look-plugins.sh
+if [ `uname` == "Darwin" ]
+then
+    ./install-homebrew-formulas.sh
+    ./install-homebrew-cask-formulas.sh
+    ./install-quick-look-plugins.sh
+    ./install-monaco-font.sh
+    ./osx-set-defaults.sh
+    ./zsh-set-as-default.sh
+else
+    sudo apt-get install zsh
+    sudo chsh -s `which zsh` $USER
+fi
 
-./install-monaco-font.sh
+touch $HOME/.vimrc_local
 ./install-vim-plugins.sh
 
-./osx-set-defaults.sh
-./zsh-set-as-default.sh
 ./zsh-cache-nuke.sh
 
 echo "All done. You should restart your terminal for new shell to take effect!"
