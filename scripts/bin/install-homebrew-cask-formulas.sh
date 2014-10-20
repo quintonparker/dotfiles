c00b58f9dcdc8d1448529dec72c03b2523cb8e51
Q <quintonparker@gmail.com>
2014-10-20 08:59:12 +0200

    Update brew cask packages

--- scripts/bin/install-homebrew-cask-formulas.sh
+++ scripts/bin/install-homebrew-cask-formulas.sh
@@ -4,12 +4,12 @@ echo "Installing homebrew cask formulas"
 
 # todo add plain cloud to cask formulas
 brew cask install caffeine
-brew cask install calibre
 brew cask install cloudapp
+brew cask install controlplane
+brew cask install cyberduck
 brew cask install dropbox
 brew cask install evernote
 brew cask install firefox
-brew cask install fluid
 brew cask install flux
 brew cask install google-chrome
 brew cask install iterm2
@@ -17,6 +17,7 @@ brew cask install lightpaper
 brew cask install java
 brew cask install kindle
 brew cask install knock
+brew cask install send-to-kindle
 brew cask install selfcontrol
 brew cask install sizeup
 brew cask install skype
