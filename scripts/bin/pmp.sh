f1b0eb3a3bd55706f7a1e7448aa0255ebe9de38d
Q <quintonparker@gmail.com>
2016-09-13 14:49:10 +0200

    Adds some colours to poor mans puppet

--- scripts/bin/pmp.sh
+++ scripts/bin/pmp.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+
 if [ $# -lt 2 ]
 then
     echo "$0 error: must supply 2 args: 1) cmd 2) list of servers eg. ./pmp.sh 'ls /tmp' 'srv01 srv02 srv03'"
     exit 1;
 fi
 
-date
-echo "Warming up Poor Man's Puppet. Vrooom"
-
 for srvr in $2
 do
-    echo "\n$srvr {"
-    echo "\n  > $1"
-    ssh -A $srvr "$1" | sed -e 's,\(.*\),  \1,g' | nl
-    echo "}"
+    printf "\n${RED}$srvr${NC} ${BLUE}{${NC}\n"
+    ssh -A $srvr "$1"
+    printf "${BLUE}}${NC}\n\n"
 done
-
-echo "\nThe End. Fanks for trying Poor Man's Puppet"
