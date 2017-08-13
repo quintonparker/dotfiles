e6d95a61b4755a3d5b96bc53bb3ca69dd54928cc
Q <quintonparker@gmail.com>
2017-08-13 10:06:52 +0200

    Update mage cache flusher

--- scripts/bin/mcache.sh
+++ scripts/bin/mcache.sh
@@ -1,2 +1,8 @@
 rm -f -r -v ~/workspace/mage/htdocs/var/cache/*
+rm -f -r -v ~/workspace/mage/htdocs/var/log/*
+rm -f -r -v ~/workspace/mage/htdocs/var/report/*
+rm -f -r -v ~/workspace/mage/htdocs/var/locks/*
+rm -f -r -v ~/workspace/mage/htdocs/var/session/*
+redis-cli -n 5 flushdb
+
 say mage cache flushed
