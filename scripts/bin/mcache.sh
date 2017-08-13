ea082a9db3253489c6d95f419bc8b87f8d03fa34
Q <quintonparker@gmail.com>
2017-08-13 11:28:25 +0200

    Say annoys ppl in the office

--- scripts/bin/mcache.sh
+++ scripts/bin/mcache.sh
@@ -4,5 +4,3 @@ rm -f -r -v ~/workspace/mage/htdocs/var/report/*
 rm -f -r -v ~/workspace/mage/htdocs/var/locks/*
 rm -f -r -v ~/workspace/mage/htdocs/var/session/*
 redis-cli -n 5 flushdb
-
-say mage cache flushed
