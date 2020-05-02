a4d351ac9542df369f4d005b42f0f9ed59fdb43d
Q <quintonparker@gmail.com>
2020-05-02 16:19:49 +0200

    Rm ol' scrappy cli source code editing toolz

--- scripts/bin/php-lint-git.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-git diff --name-only --relative | grep -E ".*\.ph(p|html)$" | while read line
-do
-    php -l "$line"
-done
--- scripts/bin/tabs-2-spaces.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ack $@ -l "\t" | while read line
-do
-      expand -4 $line > $line.new; cp $line.new $line; rm $line.new;
-done
